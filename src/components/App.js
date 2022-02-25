98a0098cf78ed8dab5cffbed595f5cadf8ac37e3
aman2222kumar <42084981+aman2222kumar@users.noreply.github.com>
2022-02-25 19:44:41 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import '../styles/App.css';
+import "../styles/App.css";
+function Number(props) {
+  return <li>{props.numb}</li>;
+}
 const App = () => {
-  
+  const [number, setNumber] = useState(1);
+  const [disabled, setdisabled] = useState(true);
+  function GoBack() {
+    setNumber((n) => n - 5);
+    if (number < 10) {
+      setdisabled(true);
+    }
+  }
+  function GoNext() {
+    setNumber((n) => n + 5);
+    setdisabled(false);
+  }
 
   return (
-    <div id="main">
-
-      <div>
-
+    <>
+      <div id="number-display">
+        <li>{number}</li>
+        <li>{number + 1}</li>
+        <li>{number + 2}</li>
+        <li>{number + 3}</li>
+        <li>{number + 4}</li>
       </div>
-    </div>
-  )
-}
-
+      <button id="back-button" onClick={GoBack} disabled={disabled}>
+        Prev Page
+      </button>
+      <button id="next-button" onClick={GoNext}>
+        Next Page
+      </button>
+    </>
+  );
+};
 
 export default App;
